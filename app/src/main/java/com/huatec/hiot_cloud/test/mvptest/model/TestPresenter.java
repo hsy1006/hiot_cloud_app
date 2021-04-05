f67dac0e1898e44657fe8da52ca3964cc4f8a583
hsy1006 <1281068244@qq.com>
2021-04-05 21:32:35 +0800

    添加MVP测试

---
+++ app/src/main/java/com/huatec/hiot_cloud/test/mvptest/model/TestPresenter.java
@@ -0,0 +1,20 @@
+package com.huatec.hiot_cloud.test.mvptest.model;
+
+
+import com.huatec.hiot_cloud.test.mvptest.TestView;
+
+public class TestPresenter {
+
+    private TestView view;
+
+    public TestPresenter(TestView view) {
+        this.view = view;
+    }
+    public void login(User user){
+        if("hsy".equals(user.getUserName()) && "123456".equals(user.getPassword())){
+            view.showMessage("登录成功");
+        }else{
+            view.showMessage("登录失败");
+        }
+    }
+}
