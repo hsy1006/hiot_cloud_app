95f5e2a4eab0d7be34b28489665136ce651444da
hsy1006 <1281068244@qq.com>
2021-05-12 15:53:06 +0800

    增加网络请求返回日志

--- app/src/main/java/com/huatec/hiot_cloud/injection/module/ApplicationModule.java
+++ app/src/main/java/com/huatec/hiot_cloud/injection/module/ApplicationModule.java
@@ -18,23 +18,27 @@ package com.huatec.hiot_cloud.injection.module;
 import android.app.Application;
 import android.content.Context;
 
-
-//import com.huatec.hiot_cloud.App;
 import com.google.gson.Gson;
 import com.huatec.hiot_cloud.App;
+import com.huatec.hiot_cloud.BuildConfig;
 import com.huatec.hiot_cloud.data.NetworkService;
 import com.huatec.hiot_cloud.injection.ApplicationContext;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+//import com.huatec.hiot_cloud.App;
 
 /**
  * Dagger module that provides objects which will live during the application lifecycle.
@@ -63,7 +67,13 @@ public class ApplicationModule {
     @Provides
     @Singleton
     OkHttpClient provideOkHttpClient(){
-        return new OkHttpClient();
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(6, TimeUnit.SECONDS);
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.level(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
+        return builder.addInterceptor(logging)
+                .cache(new Cache(new File(application.getCacheDir(), "HttpResponseCache"), 10 * 1024 * 1024))
+                .build();
     }
 
     //提供Retrofit
